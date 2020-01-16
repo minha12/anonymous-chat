7824bdab4a31c60ab06d983fc62863fe02f38929
Glitch (manonymous-chat) <none>
2020-01-16 14:35:10 +0000

    📅🦁 Checkpoint ./routes/api.js:9419671/244

--- routes/api.js
+++ routes/api.js
@@ -9,8 +9,12 @@
 'use strict';
 
 var expect = require('chai').expect;
+var ThreadHandler = require('../controllers/threadsHandler.js')
+var ReplyHandler = require('../controllers/repliesHandler.js')
 
 module.exports = function (app) {
+  var threadHandler = new ThreadHandler
+  var replyHandler = new ThreadHandler
   
   app.route('/api/threads/:board');
     
