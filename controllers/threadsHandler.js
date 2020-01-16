6a13bcbd9a404697167f8299df56eb6769cdf379
Glitch (manonymous-chat) <none>
2020-01-16 18:31:36 +0000

    ⛷🙆 Checkpoint ./controllers/threadsHandler.js:9419671/372

--- controllers/threadsHandler.js
+++ controllers/threadsHandler.js
@@ -1,5 +1,17 @@
+var mongo = require('mongodb').MongoClient
+var ObjectId = require('mongodb').ObjectID
+var url = process.env.DB
+
 function RepliesHandler() {
-  
+  this.newThread = function(req, res) {
+    var board = req.params.board
+    var thread = {
+      text: req.body.text,
+      created_on: new Date(),
+      bumped_on: new Date(),
+      
+    }
+  }
 }
 
 module.exports = RepliesHandler
