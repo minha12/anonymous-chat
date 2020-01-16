47696f17542e32fb43d57e594640c9f33a6a4fd8
Glitch (manonymous-chat) <none>
2020-01-16 19:42:39 +0000

    ☺️🕹 Checkpoint ./controllers/repliesHandler.js:9419671/979

--- controllers/repliesHandler.js
+++ controllers/repliesHandler.js
@@ -1,4 +1,4 @@
-var mogo = require('mongodb').MongoClient
+var mongo = require('mongodb').MongoClient
 var ObjectId = require('mongodb').ObjectID
 var url = process.env.DB
 
@@ -10,8 +10,38 @@ function ThreadsHandler() {
       text: req.body.text,
       created_on: new Date(),
       reported: false,
-      delete_password: req
+      delete_password: req.body.delete_password
     }
+    mongo.connect(url, (err, db) => {
+      var collection = db.collection(board)
+      collection.findAndModify(
+      {_id: new ObjectId(req.body.thread_id)},
+      [],
+      {
+        $set: {bumped_on: new Date()},
+        $push: {replies: reply}
+      })
+    })
+    res.redirect('/b/' + board + '/' + req.body.thread_id)
+  }
+  
+  this.replyList = (req, res) => {
+    var board = req.params.board
+    mongo.connect(url, (err, db) => {
+      var collection = db.collection(board)
+      collection.find(
+        {_id: new ObjectId(req.query.thread_id)},
+        {
+          reported: 0,
+          delete_password: 0,
+          'replies.delete_password': 0,
+          'replies.reported': 0
+        }
+      )
+      .toArray((err, doc) => {
+        res.json(doc[0])
+      })
+    })
   }
 }
 
