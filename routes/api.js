0b38d98c91ee6667e9df35a175d444ba38a9375a
Glitch (manonymous-chat) <none>
2025-01-09 16:36:04 +0100

    🔧✨ Load environment variables from .env file in API routes

--- routes/api.js
+++ routes/api.js
@@ -11,6 +11,7 @@
 var expect = require('chai').expect;
 var ThreadHandler = require('../controllers/threadsHandler.js')
 var ReplyHandler = require('../controllers/repliesHandler.js')
+require('dotenv').config();
 
 module.exports = function (app) {
   var threadHandler = new ThreadHandler
